5afa27c4f2f9943d0d2f11eb49d7bf1e5086012e
Max <maxfrank13@gmail.com>
2022-05-13 20:21:28 -0400

    finished home page UI/UX and polished styles

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import resume from '../assets/resume.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileLines } from '@fortawesome/free-regular-svg-icons';
 
 export default function Footer(props) {
   return (
     <footer className='w-full bg-stone-900 text-stone-100 flex justify-center gap-12 p-8 text-2xl'>
-        <a href="https://www.linkedin.com/in/maxwell-frank-b42809227/" target="_blank" rel="noreferrer">
-          <i className="devicon-linkedin-plain colored"></i>
-        </a>
-        <a href="https://twitter.com/CrayonProfessor/" target="_blank" rel="noreferrer">
-          <i className="devicon-twitter-plain colored"></i>
-        </a>
-        <a href="https://github.com/MaxFrank13" target="_blank" rel="noreferrer">
-          <i className="devicon-github-original"></i>
-        </a>
+      <a className='text-sky-700 hover:text-sky-800' href="https://www.linkedin.com/in/maxwell-frank-b42809227/" target="_blank" rel="noreferrer">
+        <i className="devicon-linkedin-plain"></i>
+      </a>
+      <a className='text-sky-500 hover:text-sky-700' href="https://twitter.com/CrayonProfessor/" target="_blank" rel="noreferrer">
+        <i className="devicon-twitter-original"></i>
+      </a>
+      <a
+        className='text-2xl text-yellow-300 hover:text-yellow-600'
+        href={resume}>
+        <FontAwesomeIcon
+          icon={faFileLines}
+        />
+      </a>
     </footer>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import resume from '../assets/resume.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileLines } from '@fortawesome/free-regular-svg-icons';
 
 export default function Contact(props) {
+
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext('2d');
+    context.scale(2,2);
+    context.lineCap = 'round';
+    context.fillStyle = '#515151';
+    context.strokeStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.lineWidth = 25;
+
+    contextRef.current = context;
+  }, []);
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({nativeEvent}) => {
+    if (!isDrawing) {
+      return;
+    };
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
   return (
-    <h1>HOME HERE</h1>
+    <section className='relative overflow-hidden'>
+      <h1 className='text-stone-100 text-3xl font-bold text-center'>Click and drag to find a hidden treasure!</h1>
+      <a 
+      className='absolute top-1/2 left-1/2 black select-none text-2xl text-stone-700 hover:text-yellow-600'
+      href={resume}>
+        <FontAwesomeIcon
+          icon={faFileLines}
+        />
+      </a>
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
+    </section>
+
   )
 }
